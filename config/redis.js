e308e9106d20f0904004b9ea4d4a56dbc1292b02
Dhineshkumar Selvam <dhivilearning@gmail.com>
2025-03-03 20:10:22 +0530

    remove: redis configuration-2

--- config/redis.js
+++ config/redis.js
@@ -1,21 +1,21 @@
-import redis, {createClient} from "redis";
-import {REDIS_PORT, REDIS_URL} from "./env.config.js"
-import {configDotenv} from "dotenv";
-
-configDotenv()
-
-//crete redis client
-const redisClient = createClient({
-    url: REDIS_URL, // Use a secure Redis URL
-    socket: {
-        reconnectStrategy: (retries) => Math.min(retries * 50, 1000), // Retry strategy
-        keepAlive: 30000, // Keep connection alive
-        connectTimeout: 10000, // Timeout if Redis doesn’t respond
-    }
-});
-
-redisClient.connect()
-    .then(() => console.log("✅ Connected to Redis"))
-    .catch((err) => console.error("Redis Connection Error:", err));
-
-export default redisClient
+// import redis, {createClient} from "redis";
+// import {REDIS_PORT, REDIS_URL} from "./env.config.js"
+// import {configDotenv} from "dotenv";
+//
+// configDotenv()
+//
+// //crete redis client
+// const redisClient = createClient({
+//     url: REDIS_URL, // Use a secure Redis URL
+//     socket: {
+//         reconnectStrategy: (retries) => Math.min(retries * 50, 1000), // Retry strategy
+//         keepAlive: 30000, // Keep connection alive
+//         connectTimeout: 10000, // Timeout if Redis doesn’t respond
+//     }
+// });
+//
+// redisClient.connect()
+//     .then(() => console.log("✅ Connected to Redis"))
+//     .catch((err) => console.error("Redis Connection Error:", err));
+//
+// export default redisClient
