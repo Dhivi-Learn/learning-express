4c9ed9e45b4884e5fcce02a056567065867e1518
Dhineshkumar Selvam <dhivilearning@gmail.com>
2025-03-03 19:26:19 +0530

    update: /api/index.js modification

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const app = require("../server");
-const serverless = require("serverless-http");
+import app from "../server";
+import  serverless from "serverless-http";
 
-module.exports = serverless(app);
+export default serverless(app);
